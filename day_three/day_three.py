980adc2cdbb5fb0365a64c8ae5c09783ba324b6a
Tim Williams <timothy.williams@thetrainline.com>
2021-12-03 11:30:03 +0000

    Added part two of day three

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 def binary_array_to_decimal(arr):
@@ -9,6 +10,23 @@ def binary_array_to_decimal(arr):
     decimal = int(bin_str, 2)
     print(decimal)
     return decimal
+
+
+def get_rating(array, position, rating):
+    assert rating == "oxygen" or rating == "c02"
+    if len(array) == 1:
+        return array
+    sums = np.sum(array, axis=0)
+    mode_threshold = math.floor(array.shape[0] / 2)
+    if rating == "oxygen":
+        modes = (sums >= mode_threshold).astype(int)
+    else:
+        modes = (sums < mode_threshold).astype(int)
+    print(position, len(array[array[:, position] == modes[position]]))
+
+    return get_rating(
+        array[array[:, position] == modes[position]], position + 1, rating
+    )
 
 
 if __name__ == "__main__":
@@ -25,3 +43,11 @@ if __name__ == "__main__":
     epsilon_dec = binary_array_to_decimal(epsilon_bin)
 
     print(gamma_dec * epsilon_dec)
+
+    oxygen_rating_bin = get_rating(a, 0, "oxygen")
+    c02_rating_bin = get_rating(a, 0, "c02")
+    print(oxygen_rating_bin)
+    print(c02_rating_bin)
+    oxygen_rating_dec = binary_array_to_decimal(oxygen_rating_bin[0])
+    c02_rating_dec = binary_array_to_decimal(c02_rating_bin[0])
+    print(oxygen_rating_dec * c02_rating_dec)
