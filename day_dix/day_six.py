e855b971e421f48359732ec7c249fff81ffd6b0c
Tim Williams <timothy.williams@thetrainline.com>
2021-12-06 20:50:40 +0000

    finished day six

--- day_dix/day_six.py
+++ day_dix/day_six.py
@@ -8,14 +8,18 @@ class FishPopulation:
         self.population_counts = {x: 0 for x in range(0, 9)}
         for v, c in zip(values, counts):
             self.population_counts[v] += c
+        self.n_iters = 0
+        print(f"Initital population counts = {self.population_counts}")
 
     def iterate_day(self):
         for v in range(0, 9):
             self.population_counts[v - 1] = self.population_counts[v]
 
         born = self.population_counts.pop(-1)
-        self.population_counts[8] += born
+        self.population_counts[8] = born
         self.population_counts[6] += born
+        self.n_iters += 1
+        print(f"After {self.n_iters} iterations")
         print(self.population_counts)
 
 
@@ -26,8 +30,10 @@ if __name__ == "__main__":
     starting_fish = np.array(all.split(","), dtype=int)
 
     world = FishPopulation(starting_fish)
-    for i in range(0, 80):
+    for i in range(1, 257):
         world.iterate_day()
-        print(i)
-        print(world.population_counts)
-    print("h")
+
+    total = 0
+    for _, c in world.population_counts.items():
+        total += c
+    print(total)
